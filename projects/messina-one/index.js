ad1ed650632ff3a92d21f7dcdff5941e9a6ea099
michaelyohanes <michaelyohanes77@gmail.com>
2022-12-27 20:47:56 +0800

    Removing hardcoded decimals

--- projects/messina-one/index.js
+++ projects/messina-one/index.js
@@ -1,80 +1,70 @@
 const { get } = require('../helper/http')
 const { fixBalancesTokens } = require('../helper/tokenMapping');
 
-const tokenDecimals = [
-  {
-    id: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    decimals: 6
-  },
-  {
-    id: '31566704',
-    decimals: 6
-  },
-  {
-    id: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-    decimals: 8
-  },
-  {
-    id: '386192725',
-    decimals: 8
-  },
-  {
-    id: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-    decimals: 18
-  },
-  {
-    id: '386195940',
-    decimals: 8
+let messinaTvls = [];
+let messinaAssets = [];
+
+const tokenChain = {
+  ethereum: 2,
+  algorand: 8
+};
+
+const endpoint = 'https://messina.one/api';
+
+const fetchTvlDetails = async() => {
+  if (!messinaTvls.length) {
+    const response = await get(`${endpoint}/stats/tvl-details`);
+
+    messinaTvls = [];
+
+    response.forEach((r) => {
+      messinaTvls.push(...r.assets);
+    });
   }
-];
 
+  return messinaTvls;
+}
 
-async function fetchData(chain) {
-  const url = 'https://messina.one/api/stats/tvl-details';
-  const response = await get(url);
-  const tvl = {};
+const fetchAssets = async () => {
+  if (!messinaAssets.length) {
+    messinaAssets = await get(`${endpoint}/bridge/get-assets?cache=true`);
+  }
 
-  response.forEach((r) => {
-    const { assets } = r;
+  return messinaAssets;
+}
 
-    assets.forEach((asset) => {
-      const { chainId, id, amount } = asset;
-      let { decimals } = asset;
-      
-      if (chainId == chain) {
-        if (chain == 2) {
-          // if decimals not found on api response, then query from tokenDecimals
-          if (!decimals) {
-            decimals = tokenDecimals.find(n => n.id == id.toLowerCase())?.decimals || 1;
-          }
+const tvlAlgo = async () => await processTvl(tokenChain.algorand);
 
-          tvl[id] = amount * (10 ** decimals);
-        } else {
-          // get coingeckoId
-          const { coingeckoId } = fixBalancesTokens.algorand[id];
+const tvlEth = async () => await processTvl(tokenChain.ethereum);
 
-          tvl[coingeckoId] = amount;
-        }
-      }
-    })
+const processTvl = async (chain) => {
+  let balances = {};
+
+  messinaTvls = await fetchTvlDetails();
+  messinaAssets = await fetchAssets();
+
+  const filteredTvls = messinaTvls.filter(t => t.chainId == chain);
+  
+  filteredTvls.forEach(f => {
+    const { id, amount }= f;
+
+    if (chain == tokenChain.ethereum) {
+      const decimals = messinaAssets.find(n => n.id == id)?.sourceDecimals || 1;
+
+      balances[id] = amount * (10 ** decimals);
+    } else {
+      // get coingeckoId
+      const { coingeckoId } = fixBalancesTokens.algorand[id];
+
+      balances[coingeckoId] = amount;
+    }
   });
 
-  return tvl;
+  return balances;
 }
-
-async function tvlEth() {
-  return await fetchData(2);
-}
-
-
-async function tvlAlgo() {
-  return await fetchData(8);
-}
-
 
 
 module.exports = {
-  start: 1665446400, // Oct 11, 2022 00:00:00 GMT as per messina bridge launch date
   ethereum: { tvl: tvlEth },
-  algorand: { tvl: tvlAlgo}
+  algorand: { tvl: tvlAlgo }
 }
